7d140e72dad003a4abdecc176701093b9ccdbc05
fmruiz <x.francoruiz@gmail.com>
2022-04-13 02:33:38 -0300

    add navbar

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,20 +1,29 @@
 import Head from "next/head";
 import React, { FC, Fragment } from "react";
 
+import { Navbar } from "../ui";
+
 interface IProps {
   children: JSX.Element | JSX.Element[];
-  title?: string
+  title?: string;
 }
 
 export const Layout: FC<IProps> = ({ children, title }: IProps) => {
   return (
     <Fragment>
       <Head>
-        <title>{title ? title : 'PokemonApp'}</title>
+        <title>{title ? title : "PokemonApp"}</title>
         <meta name="author" content="francoRuiz" />
         <meta name="description" content="Informacion sobre pokemon" />
       </Head>
-      <main>{children}</main>
+      <Navbar />
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
     </Fragment>
   );
 };
